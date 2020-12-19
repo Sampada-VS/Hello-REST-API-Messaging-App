f5f8f10b40740c7d70e7a2f0be98b22879137f3d
Sampada-VS <samssv1@gmail.com>
2020-12-19 17:04:02 +0530

    Added: welcome message

---
+++ src/main/java/com/blz/hellorestmessaging/HelloRestMessagingAppApplication.java
@@ -0,0 +1,14 @@
+package com.blz.hellorestmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestMessagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRestMessagingAppApplication.class, args);
+		System.out.println("Welcome to Hello REST API Messaging App..");
+	}
+
+}
