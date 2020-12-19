eb3ce8d9098bfadd771ba86327c7a014b74fdc63
Sampada-VS <samssv1@gmail.com>
2020-12-19 18:00:56 +0530

    Added: show hello message with name which passed as path variable

--- src/main/java/com/blz/hellorestmessaging/controller/HelloRestApiController.java
+++ src/main/java/com/blz/hellorestmessaging/controller/HelloRestApiController.java
@@ -1,5 +1,7 @@
 package com.blz.hellorestmessaging.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +17,12 @@ public class HelloRestApiController {
 	}
 	
 	@RequestMapping(value= {"/query"},method=RequestMethod.GET)
-	public String showHello(@RequestParam(value="name") String name) {
-		return "Hello "+name+" ..";
+	public String showHello(@RequestParam(value="name") String name, @RequestParam(value="lname") String lname) {
+		return "Hello "+name+" "+lname+" ..";
+	}
+	
+	@GetMapping("/param/{name}")
+	public String showHello(@PathVariable String name) {
+		return "Hello "+name+" from BridgeLabz.";
 	}
 }
