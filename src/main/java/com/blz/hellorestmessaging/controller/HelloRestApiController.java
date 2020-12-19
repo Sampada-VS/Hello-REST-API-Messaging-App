550c993faefb9d98350ad4fe32495f934526740d
Sampada-VS <samssv1@gmail.com>
2020-12-19 17:36:42 +0530

    Added: show hello message with name which passed as query parameter using get request method

--- src/main/java/com/blz/hellorestmessaging/controller/HelloRestApiController.java
+++ src/main/java/com/blz/hellorestmessaging/controller/HelloRestApiController.java
@@ -1,6 +1,8 @@
 package com.blz.hellorestmessaging.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class HelloRestApiController {
 	@RequestMapping(value= {"","/","/home"})
 	public String showHello() {
 		return "Hello From BridgeLabz";
+	}
+	
+	@RequestMapping(value= {"/query"},method=RequestMethod.GET)
+	public String showHello(@RequestParam(value="name") String name) {
+		return "Hello "+name+" ..";
 	}
 }
